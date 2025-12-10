11601a4bc440c4ab53cb4944bbd8653e59861016
NewGuy20-dev <techuse730@gmail.com>
2025-12-10 08:22:31 +0000

    Fixing errors

--- src/builder/tarExporter.ts
+++ src/builder/tarExporter.ts
@@ -5,6 +5,6 @@ import { checkCancellation } from '../utils/cancellation';
 export const exportDockerImage = async (imageName: string, buildId: string, workspacePath: string): Promise<string> => {
   const tarballPath = path.join(workspacePath, `${imageName}.tar`);
   await checkCancellation(buildId);
-  await executeCommand(`docker save -o ${tarballPath} ${imageName}`, buildId);
+  await executeCommand(`docker save -o "${tarballPath}" ${imageName}`, buildId);
   return tarballPath;
 };
